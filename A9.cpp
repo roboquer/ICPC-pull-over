092a1fc5829cc95f57abdf869473e4af37686bef
favian <favianorduna@gmail.com>
2026-04-25 17:12:29 -0600

    Problema 9A

---
+++ A9.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<numeric>
+using namespace std;
+int main(){
+    int a, b, may, min;
+    cin >> a >> b;
+    if(a>b) may = a; else may = b;
+    a = 6 - may + 1;
+    b = 6;
+    min = __gcd(a, b);
+    a = a/min;
+    b = b/min;
+    cout << a << "/" << b;
+    return 0;
+}
