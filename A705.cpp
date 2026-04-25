742c79f4132388469f022c43ee8a8898d23bf2fa
favian <favianorduna@gmail.com>
2026-04-25 16:54:26 -0600

    Problema A705

---
+++ A705.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int i;
+    for(i=1; i<=n; i++){
+        if(i==n){
+            if(i%2!=0){
+                cout << "I hate it ";
+            }else{
+                cout << "I love it ";
+            }
+        }else{
+            if(i%2!=0){
+                cout << "I hate that ";
+            }else{
+                cout << "I love that ";
+            }
+        }
+        
+    }
+    return 0;
+}
