483edafae3f7ac4668cbb52750beeace00507364
favian <favianorduna@gmail.com>
2026-04-25 15:49:01 -0600

    Codigo bueno del B2218

--- B2218.cpp
+++ B2218.cpp
@@ -3,14 +3,24 @@ using namespace std;
 int main(){
     int c;
     int sum = 0;
-    int may;
+    int may,ind;
     cin >> c;
     int ar[c][7];
     for(int i=0; i<c; i++){
         for(int j=0; j<7; j++){
             cin >> ar[i][j];
-            if(j != 6){
+        }
+    }
+    for(int i=0; i<c; i++){
+        may = ar[i][0];
+        ind = 0;
+        for(int j=1; j<7; j++){
+            if(may>ar[i][j]){
                 ar[i][j] = ar[i][j] * -1;
+            }else{
+                ar[i][ind] = ar[i][ind] * -1;
+                ind = j;
+                may = ar[i][j];
             }
         }
     }
