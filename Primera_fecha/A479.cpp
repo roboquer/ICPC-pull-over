606297afa08d517138ecd1cdd1536ecf939b8d8d
David-1221-git <merinodominguezdavid@gmail.com>
2026-05-16 00:23:46 -0600

    New structure for the git

---
+++ Primera_fecha/A479.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=0,b=0,c=0,n=4,r=0;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+   for(n=0;n=4;n++){
+    if(n=0){
+        r=a+b*c;
+    }else if (n=1)
+    {
+        i
+    }
+    
+    }
+    cout<<r;
+}
